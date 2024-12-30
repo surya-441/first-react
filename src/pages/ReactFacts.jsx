a81edb4a08760737a967a4f37bee3886554d8984
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2024-12-30 16:10:53 -0700

    added content to ReactFacts.css

--- src/pages/ReactFacts.jsx
+++ src/pages/ReactFacts.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Nav from "../Nav";
+import logo from '../assets/react-icon.png';
+import './ReactFacts.css';
 
 function ReactFacts() {
     return (
-        <div>
+        <main className="react-facts">
             <Nav />
-            <h1>Facts</h1>
-        </div>
+            <img src={logo} width="40px" alt="React Logo" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     );
 }
 
