abed42166ceff5ecbc829f50183ef9a7896a6426
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2024-12-30 11:16:53 -0700

    writing html in js without React

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById('root'))
+// const root = createRoot(document.getElementById('root'))
 
-function MainContent() {
-  return <h1>React is great</h1>
-}
-root.render(
-  <div>
-    <MainContent />
-  </div>
-)
+// function MainContent() {
+//   return <h1>React is great</h1>
+// }
+// root.render(
+//   <div>
+//     <MainContent />
+//   </div>
+// )
+
+const whyReact = document.createElement("h1");
+whyReact.textContent = "React is declarative";
+whyReact.class = "header";
+document.getElementById("root").appendChild(whyReact);
