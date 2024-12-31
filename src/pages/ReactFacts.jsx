90abae38c25fcefe43ab9c2cffd1c29d4d6b6779
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2024-12-31 14:19:45 -0700

    added styles

--- src/pages/ReactFacts.jsx
+++ src/pages/ReactFacts.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Nav from "../componenets/Nav";
 import ReactFactsMain from "../componenets/ReactFactsMain";
-import logo from '../assets/react-icon.png';
 import './ReactFacts.css';
 
 function ReactFacts() {
     return (
-        <>
+        <div className="react-facts">
             <Nav />
             <ReactFactsMain />
-        </>
+        </div>
     );
 }
 
