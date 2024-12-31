9229f1760b00de5b407a05984f278af8757b2b8e
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2024-12-31 13:51:28 -0700

    styled nav

--- src/componenets/Nav.jsx
+++ src/componenets/Nav.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import './Nav.css';
-
 function Nav() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/reactfacts">React Facts</Link>
-                </li>
-            </ul>
-        </nav>
+        <header className="header">
+            <nav className = "nav">
+                <img className="logo-img" src ="/src/assets/react-icon.png" alt="React Logo" />
+                <span className="logo-span">ReactFacts</span>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/reactfacts">React Facts</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
