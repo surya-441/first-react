342aae8b21acd002fb5fbbb165c40c6f920f50fa
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-04 11:40:12 -0700

    added nav logo and styles

--- src/componenets/TravelJournalNav.jsx
+++ src/componenets/TravelJournalNav.jsx
@@ -1,9 +1,14 @@
 import NavLinks from "./NavLinks";
+import './TravelJournalNav.css';
 
 function TravelJournalNav() {
     return (
         <header className='travel-journal-nav-header'>
             <nav className="travel-journal-nav">
+                <div className="center-logo">
+                    <img className="globe-img" src ="/src/assets/globe.png" alt="Globe Logo" />
+                    <span className="globe-span">my travel journal.</span>
+                </div>
                 <NavLinks />
             </nav>
         </header>
