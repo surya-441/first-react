8e220cd8b9c911472c72a2d4ece199498a57ba83
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-03 20:11:13 -0700

    fixed syntax

--- src/componenets/ReactFactsMain.jsx
+++ src/componenets/ReactFactsMain.jsx
@@ -3,8 +3,8 @@ import './ReactFactsMain.css';
 function ReactFactsMain() {
     return (
         <main className="react-facts-main">
-            <h1 class = "main-h1">Fun facts about React</h1>
-            <ul class = "main-ul">
+            <h1 className = "main-h1">Fun facts about React</h1>
+            <ul className = "main-ul">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 200K stars on GitHub</li>
