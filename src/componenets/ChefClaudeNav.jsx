dcf4d628aaacb5c458c18964e868faaf37f56d22
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-07 21:27:24 -0700

    styled ChefClaudeNav

--- src/componenets/ChefClaudeNav.jsx
+++ src/componenets/ChefClaudeNav.jsx
@@ -1,10 +1,14 @@
 import NavLinks from "./NavLinks";
+import chefLogo from "../assets/chef-claude.png";
+import './ChefClaudeNav.css';
 
 function ChefClaudeNav() {
     return (
-        <>
+        <header className="chef-claude-header">
+            <img className="chef-logo" src={chefLogo} alt="Chef Claude Logo" />
+            <span className="chef-logo-span">Chef Claude</span>
             <NavLinks />
-        </>
+        </header>
     );
 }
 
