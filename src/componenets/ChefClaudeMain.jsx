4518d0c5d1b4c28687828a23ce92d8a7b308ffbd
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-09 09:38:42 -0700

    added form handler

--- src/componenets/ChefClaudeMain.jsx
+++ src/componenets/ChefClaudeMain.jsx
@@ -1,16 +1,28 @@
 import './ChefClaudeMain.css';
+import { useState } from 'react';
 
 function ChefClaudeMain() {
+    const [ingredients , setIngredients] = useState([]);
+    const ingredientsLi = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>);
+    function handleAddIngredient(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        setIngredients(prevIngredients => [...prevIngredients, formData.get("ingredient")])
+    }
     return (
         <main className="chef-claude-main">
-            <form className="add-ingredient-form">
+            <form onSubmit={handleAddIngredient} className="add-ingredient-form">
                 <input 
                     aria-label="Add ingredient" 
                     placeholder="e.g. oregano"
                     type="text"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsLi}
+            </ul>
         </main>
     );
 }
