49511945f0ba568f563eb90c0da867f43f9157d0
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-10 20:43:44 -0700

    refactored ChefClaudeMain

---
+++ src/componenets/ChefClaudeIngredientsList.jsx
@@ -0,0 +1,20 @@
+function ChefClaudeIngredientsList(props) {
+    const ingredientsLi = props.ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>);
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list">{ingredientsLi}</ul>
+            {   props.ingredients.length > 3 && 
+                <div className='get-recipe-container'>
+                    <div>
+                        <h3>Ready for a recipe?</h3>    
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.getRecipe}>Get a Recipe</button>
+                </div> 
+            }
+        </section>
+    );
+}
+
+export default ChefClaudeIngredientsList;
--- src/componenets/ChefClaudeMain.jsx
+++ src/componenets/ChefClaudeMain.jsx
@@ -1,11 +1,16 @@
 import './ChefClaudeMain.css';
 import { useState } from 'react';
+import ChefClaudeRecipe from './ChefClaudeRecipe';
+import ChefClaudeIngredientsList from './ChefClaudeIngredientsList';
 
 function ChefClaudeMain() {
-    const [ingredients , setIngredients] = useState([]);
-    const ingredientsLi = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>);
+    const [ingredients, setIngredients] = useState([]);
+    const [recipeShown, setRecipeShown] = useState(false);
     function addIngredient(formData) {
         setIngredients(prevIngredients => [...prevIngredients, formData.get("ingredient")])
+    }
+    function getRecipe() {
+        setRecipeShown(prev => !prev);
     }
     return (
         <main className="chef-claude-main">
@@ -18,21 +23,8 @@ function ChefClaudeMain() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {   ingredients.length > 0 &&
-                <section>
-                    <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list">{ingredientsLi}</ul>
-                    {   ingredients.length > 3 && 
-                        <div className='get-recipe-container'>
-                            <div>
-                                <h3>Ready for a recipe?</h3>    
-                                <p>Generate a recipe from your list of ingredients.</p>
-                            </div>
-                            <button>Get a Recipe</button>
-                        </div> 
-                    }
-                </section>
-            }
+            {ingredients.length > 0 && <ChefClaudeIngredientsList ingredients={ingredients} getRecipe={getRecipe}/>}
+            {recipeShown && <ChefClaudeRecipe />}
         </main>
     );
 }
