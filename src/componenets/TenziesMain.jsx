4b9adc5156da39d55239bea2fd41205c1fd45827
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-17 16:21:46 -0700

    add isHeld property to die

--- src/componenets/TenziesMain.jsx
+++ src/componenets/TenziesMain.jsx
@@ -1,13 +1,20 @@
 import TenziesDie from './TenziesDie';
 import './TenziesMain.css';
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function TenziesMain() {
     function generateAllNewDice() {
-        return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            }));
     }
     const [diceArray, setDiceArray] = useState(generateAllNewDice());
-    const tenziesDieArr = diceArray.map((val) => <TenziesDie value={val} />)
+    const tenziesDieArr = diceArray.map((die) => <TenziesDie key={die.id} value={die.value} />)
     return (
         <main className="tenzies-main">
             <div className="dice-container">
