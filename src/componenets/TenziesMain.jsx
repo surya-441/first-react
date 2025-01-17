03a8a7a847303511929c6efa46a0284633f0efcd
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-17 16:08:23 -0700

    generate 10 dice with random numbers

--- src/componenets/TenziesMain.jsx
+++ src/componenets/TenziesMain.jsx
@@ -1,21 +1,19 @@
 import TenziesDie from './TenziesDie';
 import './TenziesMain.css';
+import { useState } from 'react';
 
 function TenziesMain() {
+    function generateAllNewDice() {
+        return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    }
+    const [diceArray, setDiceArray] = useState(generateAllNewDice());
+    const tenziesDieArr = diceArray.map((val) => <TenziesDie value={val} />)
     return (
         <main className="tenzies-main">
             <div className="dice-container">
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-                <TenziesDie value={0} />
-            </div>
+                {...tenziesDieArr}
+           </div>
+           <button className="roll-dice" onClick={()=>setDiceArray(generateAllNewDice())}>Roll</button>
         </main>
     );
 }
