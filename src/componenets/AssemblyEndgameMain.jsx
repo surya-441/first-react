254c7e52bbb5c3ab5c7a69951dd860ce29dd7215
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-21 12:49:41 -0700

    added currentWord

--- src/componenets/AssemblyEndgameMain.jsx
+++ src/componenets/AssemblyEndgameMain.jsx
@@ -1,12 +1,19 @@
 import './AssemblyEndgameMain.css';
 import { languages } from '../languages';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 function AssemblyEndgameMain() {
+    const [ currentWord, setCurrentWord ] = useState('react');
     const languageElements = languages.map((language) => 
         <span  key={nanoid()}
             style={{backgroundColor: language.backgroundColor, color: language.color}}
         >
             {language.name}
+        </span>
+    )
+    const currentWordArray = currentWord.split("").map((ch) => 
+        <span key={nanoid()}>
+            {ch.toUpperCase()}
         </span>
     )
     return (
@@ -17,6 +24,9 @@ function AssemblyEndgameMain() {
             </section>
             <section className='language-chips'>
                 {languageElements}
+            </section>
+            <section className='current-word'>
+                {currentWordArray}
             </section>
         </main>
     );
