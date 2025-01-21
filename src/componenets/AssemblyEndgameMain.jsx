40263a1219b601cc64339fcbe54eef3004cc4399
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-21 12:15:51 -0700

    display languages

--- src/componenets/AssemblyEndgameMain.jsx
+++ src/componenets/AssemblyEndgameMain.jsx
@@ -1,11 +1,22 @@
 import './AssemblyEndgameMain.css';
-
+import { languages } from '../languages';
+import { nanoid } from 'nanoid';
 function AssemblyEndgameMain() {
+    const languageElements = languages.map((language) => 
+        <span  key={nanoid()}
+            style={{backgroundColor: language.backgroundColor, color: language.color}}
+        >
+            {language.name}
+        </span>
+    )
     return (
         <main className='assembly-endgame-main'>
             <section className='game-status'>
                 <h2>You Win!</h2>
                 <p>Well done! 🎉</p>
+            </section>
+            <section className='language-chips'>
+                {languageElements}
             </section>
         </main>
     );
