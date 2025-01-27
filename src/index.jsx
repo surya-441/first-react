b62775f1136bd0af9c8525e7b598a361f3b06a9c
Surya Siddharthan Sivakumar <surya040401@gmail.com>
2025-01-26 17:41:09 -0700

    added strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { StrictMode } from 'react';
 const root = createRoot(document.getElementById('root'))
 
 root.render(
+    <StrictMode>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </StrictMode>
 )
